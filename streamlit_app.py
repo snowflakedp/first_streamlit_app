8ebe05735726d1ed26fd9e834ccd8acaccd8efc1
snowflakedp <110241849+snowflakedp@users.noreply.github.com>
2022-08-02 15:43:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_rows)
 #allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#add user input to snowflake database
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
